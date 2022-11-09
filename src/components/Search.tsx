fb99e8871c2c27fecdd4cf3c4db5372198f0ac49
jaeyeong815 <jae0ee@naver.com>
2022-11-09 14:32:33 +0900

    style: 사용하지 않는 ref태그 제거

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,19 +1,15 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import { FiSearch } from 'react-icons/fi';
 import { StSearch, StTitle, InputWrap, StInput } from './Search.style';
 
 const Search = () => {
-  const inputRef = useRef<HTMLInputElement>(null);
-  const handleFocus = () => {
-    inputRef.current?.focus();
-  };
   return (
     <StSearch>
       <StTitle>
         국내 모든 임상시험 검색하고 <br />
         온라인으로 참여하기
       </StTitle>
-      <InputWrap ref={inputRef}>
+      <InputWrap>
         <FiSearch />
         <StInput type="search" placeholder="질환명을 입력해 주세요." />
       </InputWrap>
