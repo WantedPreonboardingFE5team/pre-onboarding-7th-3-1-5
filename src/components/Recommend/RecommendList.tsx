03b4d6a663596ba2bd01449a50b83f1a59bd06f8
jaeyeong815 <jae0ee@naver.com>
2022-11-10 13:19:02 +0900

    refactor: isLoading 기본값 변경

--- src/components/Recommend/RecommendList.tsx
+++ src/components/Recommend/RecommendList.tsx
@@ -9,14 +9,13 @@ import NoRecommendList from './NoRecommendList';
 import Loading from './Loading';
 
 const RecommendList = () => {
-  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
   const [recommendList, setRecommendList] = useState<IIllness[]>([]);
 
   const keyword = useRecoilValue(keywordState);
   // TODO recommendList 개수 제한하기 (10개?)
   // TODO 키보드로 추천 검색어 이동가능하게 하기
   useEffect(() => {
-    setIsLoading(true);
     setRecommendList([]); // 이 과정이 없으면 이전 추천 목록이 남아있어서 로딩중 아래에 데이터가 나와있음
     // api 호출이 여러번 되는 것을 막기위해 setTimeout을 사용하여 debounce 처리
     const debounce = setTimeout(
@@ -30,7 +29,6 @@ const RecommendList = () => {
     );
     return () => clearInterval(debounce);
   }, [keyword]);
-  console.log('recommendList', recommendList);
 
   return (
     <>
