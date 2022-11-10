a098a721a1778d9b85a7c55e66d3d779ea2ae957
jaeyeong815 <jae0ee@naver.com>
2022-11-10 13:17:02 +0900

    refactor: 구현해둔 기능이 더 관련있는 컴포넌트로 이동

--- src/components/Recommend/Recommend.tsx
+++ src/components/Recommend/Recommend.tsx
@@ -1,48 +1,12 @@
-import React, { useEffect, useState } from 'react';
-import { useRecoilValue } from 'recoil';
-import { IIllness } from '../../types/illness';
-import fetchSearchData from '../../apis';
+import React from 'react';
 import { StRecommend, StRecommendTitle } from './Recommend.style';
-import { keywordState } from '../../recoil/keywordState';
-import Loading from './Loading';
-import NoRecommendList from './NoRecommendList';
 import RecommendList from './RecommendList';
 
 const Recommend = () => {
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [recommendList, setRecommendList] = useState<IIllness[]>([]);
-
-  const keyword = useRecoilValue(keywordState);
-
-  // TODO recommendList 개수 제한하기 (10개?)
-  // TODO 키보드로 추천 검색어 이동가능하게 하기
-  useEffect(() => {
-    setIsLoading(true);
-    setRecommendList([]); // 이 과정이 없으면 이전 추천 목록이 남아있어서 로딩중 아래에 데이터가 나와있음
-    // api 호출이 여러번 되는 것을 막기위해 setTimeout을 사용하여 debounce 처리
-    const debounce = setTimeout(
-      () =>
-        keyword &&
-        fetchSearchData(keyword).then((result) => {
-          setRecommendList(result);
-          setIsLoading(false);
-        }),
-      500,
-    );
-    return () => clearInterval(debounce);
-  }, [keyword]);
-
-  // TODO context api 또는 redux, recoil 등을 사용하면 분리 가능 (로컬캐싱 구현 필요)
-  const RecommendListComp = recommendList.map((recommend: IIllness) => {
-    return <RecommendList key={recommend.sickCd} sickCd={recommend.sickCd} sickNm={recommend.sickNm} />;
-  });
-
   return (
     <StRecommend>
       <StRecommendTitle>추천 검색어</StRecommendTitle>
-      {isLoading && <Loading />}
-      {RecommendListComp}
-      {!isLoading && RecommendList.length === 0 && <NoRecommendList />}
+      <RecommendList />
     </StRecommend>
   );
 };
--- src/components/Recommend/RecommendList.tsx
+++ src/components/Recommend/RecommendList.tsx
@@ -1,23 +1,58 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useRecoilValue } from 'recoil';
 import { FiSearch } from 'react-icons/fi';
 import { keywordState } from '../../recoil/keywordState';
+import { IIllness } from '../../types/illness';
 import { StRecommendContent } from './Recommend.style';
+import fetchSearchData from '../../apis';
+import NoRecommendList from './NoRecommendList';
+import Loading from './Loading';
 
-const RecommendList = ({ sickCd, sickNm }: { sickCd: string; sickNm: string }) => {
+const RecommendList = () => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [recommendList, setRecommendList] = useState<IIllness[]>([]);
+
   const keyword = useRecoilValue(keywordState);
-  const prevKeword = sickNm.split(keyword)[0];
-  const nextKeword = sickNm.split(keyword)[1];
+  // TODO recommendList 개수 제한하기 (10개?)
+  // TODO 키보드로 추천 검색어 이동가능하게 하기
+  useEffect(() => {
+    setIsLoading(true);
+    setRecommendList([]); // 이 과정이 없으면 이전 추천 목록이 남아있어서 로딩중 아래에 데이터가 나와있음
+    // api 호출이 여러번 되는 것을 막기위해 setTimeout을 사용하여 debounce 처리
+    const debounce = setTimeout(
+      () =>
+        keyword &&
+        fetchSearchData(keyword).then((result) => {
+          setRecommendList(result);
+          setIsLoading(false);
+        }),
+      500,
+    );
+    return () => clearInterval(debounce);
+  }, [keyword]);
+  console.log('recommendList', recommendList);
 
   return (
-    <StRecommendContent key={sickCd}>
-      <FiSearch />
-      <p>
-        {prevKeword}
-        <span>{keyword}</span>
-        {nextKeword}
-      </p>
-    </StRecommendContent>
+    <>
+      {isLoading && <Loading />}
+      {recommendList &&
+        recommendList.map((recommend: IIllness) => {
+          const prevKeword = recommend.sickNm.split(keyword)[0];
+          const nextKeword = recommend.sickNm.split(keyword)[1];
+
+          return (
+            <StRecommendContent key={recommend.sickCd}>
+              <FiSearch />
+              <p>
+                {prevKeword}
+                <span>{keyword}</span>
+                {nextKeword}
+              </p>
+            </StRecommendContent>
+          );
+        })}
+      {!isLoading && recommendList.length === 0 && <NoRecommendList />}
+    </>
   );
 };
 
